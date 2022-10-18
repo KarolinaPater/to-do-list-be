ffa53ced04fba1d799034fd613ffdab95cda0d09
Karolina Pater <karolinapater1995@gmail.com>
2022-10-18 13:13:57 +0200

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  db: "mongodb+srv://Admin1:XDaTtD4VrCLALYkI@cluster0.cpowwxf.mongodb.net/?retryWrites=true&w=majority",
+  keySession: ["TuJestKlucz"],
+  maxAgeSession: 24 * 60 * 1000,
+  JWT_SECRET: "jwtlosowyciagznakow",
+};
