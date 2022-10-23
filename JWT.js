97b3b8a6a8ff005fdd34b5e5afe45263d12f3afb
Karolina Pater <karolinapater1995@gmail.com>
2022-10-23 13:14:09 +0200

    session

--- JWT.js
+++ JWT.js
@@ -4,9 +4,9 @@ const { sign, verify } = require("jsonwebtoken");
 
 const createTokens = (user) => {
   const accessToken = sign(
-    { fullname: user.fullname, id: user.id },
+    { email: user.email, id: user.id },
     config.JWT_SECRET,
-    { expiresIn: 2400 }
+    { expiresIn: "6h" }
   );
   return accessToken;
 };
@@ -14,17 +14,19 @@ const validateToken = (req, res, next) => {
   const accessToken = req.headers["x-access-token"];
 
   if (!accessToken) {
-    console.log("Uzytkownik nie posiada autoryzacji");
-    return res.json({ error: "Uzytkownik nie posiada autoryzacji" });
+    return res
+      .status(400)
+      .json({ error: "Uzytkownik nie posiada autoryzacji" });
   }
   try {
     const validToken = verify(accessToken, config.JWT_SECRET);
     if (validToken) {
-      console.log("autoryzacja powiodla sie!");
       return next();
     }
   } catch (err) {
-    return res.json({ error: "Uzytkownik nie posiada autoryzacji" });
+    return res
+      .status(400)
+      .json({ error: "Uzytkownik nie posiada autoryzacji" });
   }
 };
 module.exports = { createTokens, validateToken };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const sha512 = require("js-sha512");
-
-const { createTokens } = require("../JWT");
+const config = require("../config");
+const { verify } = require("jsonwebtoken");
+const { createTokens, validateToken } = require("../JWT");
 
 const User = require("../models/user");
 
@@ -11,19 +12,19 @@ router.get("/", function (req, res, next) {
 });
 
 router.post("/register", (req, res) => {
-  console.log('req body w rejestracji: ', req.body);
-  const { name, last_name, email, password } =
-    req.body;
+  const { name, last_name, email, password } = req.body;
 
   if (!name || !last_name || !email || !password) {
     res.json(400, { message: "Prosze uzupełnić wymagane pola" });
   }
-  if (password.length < 8) res.json(400, { message: "Hasło musi sie składać z minimum 8 znaków" });
-  if (email.length > 250) res.json(400, { message: "Email może zawierac maksymalnie 60 znaków" });
+  if (password.length < 8)
+    res.json(400, { message: "Hasło musi sie składać z minimum 8 znaków" });
+  if (email.length > 250)
+    res.json(400, { message: "Email może zawierac maksymalnie 60 znaków" });
 
   User.findOne({ email: email }).then((user) => {
     if (user) {
-      res.json(400, { message: 'Ten email jest już w użyciu.' });
+      res.json(400, { message: "Ten email jest już w użyciu." });
     } else {
       const newUser = new User({
         name,
@@ -32,19 +33,20 @@ router.post("/register", (req, res) => {
         password,
       });
       newUser.password = sha512(newUser.password);
-      newUser.save().then((user) => {
-        console.log("Rejestracja powiodła się");
-        res.json({
-          message: "Rejestracja powiodła się",
-        });
-      }).catch((err) => res.json(400, err));
+      newUser
+        .save()
+        .then((user) => {
+          res.json({
+            message: "Rejestracja powiodła się",
+          });
+        })
+        .catch((err) => res.json(400, err));
     }
   });
 });
 
 router.post("/login", (req, res) => {
   const { email, password } = req.body;
-  console.log('dane z fronta', req.body);
   const hashpassword = sha512(password);
 
   if (!email) res.json(400, { message: "Prosze uzupełnić email" });
@@ -58,11 +60,46 @@ router.post("/login", (req, res) => {
       res.json(400, { message: "Niewłasciwy email lub hasło" });
     } else {
       const accessToken = createTokens(user);
-      res.json({
+      res.json(200, {
         message: "Zostałeś prawidłowo zalogowany",
         is_logged: true,
         accessToken,
-        user,
+        user: {
+          name: user.name,
+          last_name: user.last_name,
+          email: user.email,
+        },
+      });
+    }
+  });
+});
+
+router.post("/logout", validateToken, (req, res) => {
+  console.log("wylgoggowywuje");
+  res.json(200, { message: "Wylogowywanie" });
+});
+
+router.post("/session", validateToken, (req, res) => {
+  const accessToken = req.headers["x-access-token"];
+  const token = verify(accessToken, config.JWT_SECRET);
+
+  const user = User.findOne({
+    email: token.email.toLowerCase(),
+    _id: token.id,
+  }).then((user) => {
+    if (!user) {
+      res.json(400, { message: "Niewłasciwy email lub id" });
+    } else {
+      const accessToken = createTokens(user);
+      res.json(200, {
+        message: "sesja odswiezona",
+        is_logged: true,
+        accessToken,
+        user: {
+          name: user.name,
+          last_name: user.last_name,
+          email: user.email,
+        },
       });
     }
   });
